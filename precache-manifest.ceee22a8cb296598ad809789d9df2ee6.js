1ce1ae3f2f7ed1f9731df3b1dca0d069068b769c
Laith Younes <laith.younes@optimumpartners.co>
2020-07-23 18:06:46 +0300

    Updates

---
+++ precache-manifest.ceee22a8cb296598ad809789d9df2ee6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f88784e92ad3491724ce07b755fa55a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1fe6b3b396c4862c17f9",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "6b73445ab50a6db9d1c5",
+    "url": "./static/js/2.cde98cbe.chunk.js"
+  },
+  {
+    "revision": "73556b140791e7f1e95a61af7da7dddb",
+    "url": "./static/js/2.cde98cbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fe6b3b396c4862c17f9",
+    "url": "./static/js/main.c84c5b49.chunk.js"
+  },
+  {
+    "revision": "3a4cd0861ffd0aa10c7c",
+    "url": "./static/js/runtime-main.1b27f219.js"
+  }
+]);
